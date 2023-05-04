1d87c4464e46156f339ec588150f8697860ffb04
vijay <vijayanandraj@gmail.com>
2023-05-04 21:45:48 +0530

    htmlutil.java

--- src/test/java/com/vijay/scoringengine/util/HtmlUtil.java
+++ src/test/java/com/vijay/scoringengine/util/HtmlUtil.java
@@ -6,7 +6,6 @@ public class HtmlUtil {
 
     private static final String REGEX_PATTERN = "xlink:href\\s*=\\s*\"([^\"]*background\\.png)\"";
     private static final Pattern PATTERN = Pattern.compile(REGEX_PATTERN);
-    //private static final String REPLACEMENT = "xlink:href=\"http://localhost/assests/images/test.png\"";
 
     public static String replaceBackgroundImage(String htmlString, String newUrl) {
         String replacement = "xlink:href=\"" + newUrl + "\"";
